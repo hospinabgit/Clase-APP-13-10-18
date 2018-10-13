2abc899ff157d88db5189d840902a7972dbb0d00
hospinab <hospinab@gmail.com>
2018-10-13 14:26:23 -0500

    inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 
-  root "welcome#index"
+  Rails.application.routes.draw do
+    root "welcome#index"
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     namespace :api do
       post 'auth' => 'user_token#create'
